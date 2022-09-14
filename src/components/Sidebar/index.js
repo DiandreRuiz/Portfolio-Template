7cfe47cc46fb8db59fa8a84cca6b3aaa390b5119
Diandre Ruiz <111468956+DiandreRuiz@users.noreply.github.com>
2022-09-14 12:05:19 -0400

    Added social media buttons and external links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,20 +1,24 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
 //import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+//import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 //import {Bandlez_logo} from '../../assets/images/Bandlez_logo.png'
-import { faEnvelope, faHome, faUser, fa} from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 //Fix <img> source below//
 const Sidebar = () => (
    <div className="nav-bar">
       <Link className="logo" to="/">
-         <img src="/images/Bandlez_logo.png" alt="logo" /> 
-         <img className="sub-logo" src="/images/Bandlez_txt.png" alt="slobodan" />
+         <img src="/images/Bandlez_logo.png" alt="logo" />
+         <img
+            className="sub-logo"
+            src="/images/Bandlez_txt.png"
+            alt="slobodan"
+         />
       </Link>
       <nav>
          <NavLink exact="true" activeclassname="active" to="/">
-            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            <FontAwesomeIcon icon={faHome} color="#9999" />
          </NavLink>
          <NavLink
             exact="true"
@@ -22,7 +26,7 @@ const Sidebar = () => (
             className="about-link"
             to="/about"
          >
-            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            <FontAwesomeIcon icon={faUser} color="#9999" />
          </NavLink>
          <NavLink
             exact="true"
@@ -30,16 +34,56 @@ const Sidebar = () => (
             className="contact-link"
             to="/contact"
          >
-            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            <FontAwesomeIcon icon={faEnvelope} color="#9999" />
          </NavLink>
       </nav>
-      <ul>
+      <div className="wrapper">
+         <ul>
          <li>
-            <a target="_blank" rel="noreferrer" href="https://www.facebook.com/Bandlez">
-               
-            </a>
-         </li>
-      </ul>
+               <a
+                  name="Spotify"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://open.spotify.com/artist/7kIxfmjYGQui2LCqXGLfWA?si=tPhvq0XFSpCThhp1dIdJFQ"
+               >
+                  <img
+                     className="social_icon"
+                     src="/images/spotify_icon.png"
+                     alt="logo"
+                  />
+               </a>
+            </li>
+            <li>
+               <a
+                  name="Facebook"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://Facebook.com/Bandlez"
+               >
+                  <img
+                     className="social_icon"
+                     src="/images/facebook_icon.png"
+                     alt="logo"
+                  />
+               </a>
+            </li>
+            <li>
+               <a
+                  name="Github"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/DiandreRuiz"
+               >
+                  <img
+                     className="social_icon"
+                     src="/images/github_icon.png"
+                     alt="logo"
+                  />
+               </a>
+            </li>
+            
+         </ul>
+      </div>
    </div>
 )
 
